8401c4b9da89998935cecea2f075e2564c534b54
Alfred Cunee <43733997+MyaushkaSH@users.noreply.github.com>
2023-11-22 23:17:38 +0300

    Init commit

---
+++ src/main/java/com/myaushka/com/frank/FrankApplication.java
@@ -0,0 +1,13 @@
+package com.myaushka.com.frank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/myaushka/com/frank/FrankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myaushka.com.frank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
